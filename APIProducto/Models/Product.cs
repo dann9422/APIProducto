b072147f388de32f83f85cf85c2841f7961fb8ae
dann9422 <dannsk82294@gmail.com>
2023-06-03 18:27:42 -0600

    Agregar archivos de proyecto.

---
+++ APIProducto/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace APIProducto.Models
+{
+    public class Product
+    {
+
+        public int IdProduct { get; set; }
+        public int CodigoBarra { get; set; }
+        public string Nombre { get; set; }
+        public string Marca { get; set; }
+        public string Categoria { get; set; }
+        public float Precio { get; set; }
+    }
+}
